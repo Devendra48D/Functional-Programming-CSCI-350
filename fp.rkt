de057c73e47e356aac751ff700eb792ad47eb142
Devendra Shah <devendrashah97@gmail.com>
2017-01-20 16:54:35 -0500

    reverse-general function started with comments

---
+++ fp.rkt
@@ -0,0 +1,6 @@
+;;;Function reverse-general takes one parameter, a list L which can contain any number
+;;;of elements and can also contain nested lists; returns a new list which is a reversed version of L.
+;;;All the sub lists in L are reversed as well. 
+
+(DEFINE (reverse-general L)
+)

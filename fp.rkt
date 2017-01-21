b54db2f37b2589433c08e816c8b973f9fb604115
Devendra Shah <devendrashah97@gmail.com>
2017-01-21 17:16:42 -0500

    Condition when the first element is not a number; and logic added

--- fp.rkt
+++ fp.rkt
@@ -1,12 +1,12 @@
-;;;Function reverse-general takes one parameter, a list L which can contain any number
-;;;of elements and can also contain nested lists; returns a new list which is a reversed version of L.
-;;;All the sub lists in L are reversed as well. 
+;;; Function reverse-general takes one parameter, a list L which can contain any number
+;;; of elements and can also contain nested lists; returns a new list which is a reversed version of L.
+;;; All the sub lists in L are reversed as well. 
 
-;;;Logic: -returns empty list if L is empty
+;;; Logic: -returns empty list if L is empty
 ;;;       -if the first element in the list is a list then reverses it and
 ;;; appends the result to the end of the rest of the list
 ;;;       - if L is a simple list then appends the first element to the end of the rest of the list until
-;;;L is reversed
+;;; L is reversed
 
 (DEFINE (reverse-general L)
     (COND
@@ -16,14 +16,22 @@
     )
 )
 
-;;;Function sum-up-numbers-simple takes one parameter, a list L which may contain
-;;;numbers and non-numbers as elements; returns the sum of the numbers not in
-;;;nested lists in L. If there are no such numbers, the result is 0.
+;;; Function sum-up-numbers-simple takes one parameter, a list L which may contain
+;;; numbers and non-numbers as elements; returns the sum of the numbers not in
+;;; nested lists in L. If there are no such numbers, the result is 0.
+
+;;; Logic: -If the list is empty answer is 0
+;;;        - If the first element is a list, answer is (0 + the sum of rest of the list)
+;;;        - If the first element is not a number, answer is (0 + the sum of rest of the list)
+;;;        - If the first elemet is a number, answer is (first element + the sum of the rest of the list)
 
 (DEFINE (sum-up-numbers-simple L)
     (COND
         ((NULL? L) 0) ;;empty list
         ((LIST? (CAR L)) (+ 0 (sum-up-numbers-simple (CDR L)))) ;;first element is a list
+        ((NOT (NUMBER? (CAR L))) (+ 0 (sum-up-numbers-simple (CDR L)))) ;; first element is not a number
         ((NUMBER? (CAR L)) (+ (CAR L) (sum-up-numbers-simple (CDR L)))) ;;first element is a number
     )
 )
+
+
